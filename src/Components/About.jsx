c1bf7efcceafda7fd2b8bd2b4a09d3dd76b467b7
Pierce <info@sadly.link>
2024-04-25 08:07:19 +0000

    updated about me section and images

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a computer science student. I enjoy coding but also wiritng and everything connected to storytelling. This site is a work in progress!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
 ];
 
 /**
@@ -44,8 +38,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "I am in the charming state of confusion - Ada Lovelace";
 
 const About = () => {
   return (
